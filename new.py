dbc997389ce175902a13459b8f0cf3b15832943c
Ashish Chaudhary <ashishchaudhary62@gmail.com>
2026-02-27 09:37:17 +0000

    this is version 1 commit

--- new.py
+++ new.py
@@ -1,4 +1,9 @@
-def hello(name):
-    print(f"Hello, {name}")
+import sys
 
-hello("Ashish")
+def hi(list_of_names):
+    print(f"Hi my first cohort students are: {list_of_names}")
+
+if __name__ == "__main__":
+    print("Version 1")
+    names = sys.argv[1:]   # everything after filename
+    hi(names)
